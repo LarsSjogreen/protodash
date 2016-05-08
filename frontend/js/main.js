de65a5ef8ff6b88f5a791f4bda8c4b368528d13e
Lars Sjögreen <lars.sjogreen@akvo.org>
2016-05-08 20:40:03 +0200

    Testing to load from github api with ajax.

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -2,10 +2,22 @@ var sp = new StatusPage.page({ page : '9myn2pbrw2yg' });
 var stat = document.getElementById('status');
 sp.status({
   success : function(data) {
-  	stat.innerHTML = "Banajnas";
   	stat.innerHTML = data.status.description;
   }
 });
+
+function callAjax(url, callback){
+    var xmlhttp;
+    // compatible with IE7+, Firefox, Chrome, Opera, Safari
+    xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function(){
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200){
+            callback(xmlhttp.responseText);
+        }
+    }
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+}0
 
 window.onload = function(){
 	var closeButts = document.getElementsByClassName('close');
@@ -16,3 +28,13 @@ window.onload = function(){
 	    };
 	}
 };
+
+function loadteam() {
+  var teamsters = document.getElementById("akvoteam");
+  teamsters.innerHTML = "Loading...";
+  callAjax("/checkReal", function(data) {
+    teamsters.innerHTML = data;
+  })
+}
+
+loadteam();
